fca5339ee4ca6db7e40f2196e8e1f22138d610f7
maomaocun <yangyicun187@gmail.com>
2025-05-24 10:43:16 +0800

    update code

--- utils/utils.py
+++ utils/utils.py
@@ -1,15 +1,6 @@
 import torch
 import random
 import numpy as np
-import torch.nn.functional as F
-from torch.distributed._tensor import Shard, Replicate
-from torch.distributed.tensor.parallel import (
-    ColwiseParallel, 
-    RowwiseParallel, 
-    SequenceParallel,
-    PrepareModuleInput,
-    parallelize_module,
-)
 def set_seed(seed):
     torch.manual_seed(seed)
     random.seed(seed)
