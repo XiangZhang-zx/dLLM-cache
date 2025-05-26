c9be3fc25e1a86e6a8a88744e7999437daa85027
yaro1214 <15934071030@163.com>
2025-05-26 20:37:37 +0800

    update scripts

--- scripts/run_LLaDA_bbh_Instruct.sh
+++ scripts/run_LLaDA_bbh_Instruct.sh
@@ -5,6 +5,8 @@ accelerate launch --config_file accelerate_config.yaml evaluation_script.py -m l
 --output_path ./bbh_log \
 --log_samples \
 --trust_remote_code \
+--apply_chat_template \
+--fewshot_as_multiturn \
 
 accelerate launch --config_file accelerate_config.yaml evaluation_script.py -m lm_eval --model LLaDA --tasks bbh --batch_size 2 \
 --model_args "pretrained=GSAI-ML/LLaDA-8B-Instruct,prompt_interval_steps=50,gen_interval_steps=6,cfg_interval_steps=1,transfer_ratio=0.25,is_feature_cache=True,is_cfg_cache=False" \
@@ -13,5 +15,8 @@ accelerate launch --config_file accelerate_config.yaml evaluation_script.py -m l
 --output_path ./bbh_log \
 --log_samples \
 --trust_remote_code \
+--apply_chat_template \
+--fewshot_as_multiturn \
+
 
 
